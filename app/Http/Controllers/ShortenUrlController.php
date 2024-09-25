4bad1a3f6bac446d1f0ea5012c5a0eccf8214ba9
Tobi Anifowose <tobi.anifowose@internetbrands.com>
2024-09-25 19:40:26 +0100

    Initial commit

---
+++ app/Http/Controllers/ShortenUrlController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShortUrl;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class ShortenUrlController
+{
+    public function index()
+    {
+        return view('welcome', [
+            'urls' => ShortUrl::latest()->paginate(30),
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        ShortUrl::create([
+            'url' => $request->get('url'),
+            'short_url' => Str::random(7),
+        ]);
+
+        return redirect()->route('url.index')->with(['status' => 'Url created successfully']);
+    }
+
+    public function show(ShortUrl $url)
+    {
+        return redirect()->away($url->url);
+    }
+
+    public function destroy(ShortUrl $url)
+    {
+        $url->delete();
+
+        return redirect()->route('url.index');
+    }
+}
---
+++ tests/Feature/ShortenUrlTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ShortenUrlTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_create_shorten_url(): void
+    {
+        $this->post('/', [
+            'url' => 'https://example.test',
+        ])->assertRedirectToRoute('url.index');
+
+        $this->assertDatabaseHas('short_urls', ['url' => 'https://example.test']);
+    }
+}
